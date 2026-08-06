4ec01a5d0275cc7c70fe59f0c25039e8fdb0a7bb
5aaryan6k <aaryankoirala20@gmail.com>
2026-08-06 21:36:39 +0545

    mm

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+// @ts-expect-error CSS side-effect import is handled by the build tool.
 import "./index.css";
 import App from "./App";
 
